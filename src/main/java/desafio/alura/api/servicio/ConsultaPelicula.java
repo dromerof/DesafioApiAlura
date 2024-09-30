2cb61ff59023ddf667a512f7bcd81002334c1ef9
Daniel Romero <dandres508@gmail.com>
2024-09-30 17:32:59 -0500

    Gson agregado

--- src/main/java/desafio/alura/api/servicio/ConsultaPelicula.java
+++ src/main/java/desafio/alura/api/servicio/ConsultaPelicula.java
@@ -1,20 +1,30 @@
 package desafio.alura.api.servicio;
 
+import com.google.gson.Gson;
 import desafio.alura.api.modelos.Pelicula;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConsultaPelicula {
-    Pelicula buscarPelicula(int numeroDePelicula) {
-        URI.create("https://swapi.py4e.com/api/films/"+numeroDePelicula+"/");
+
+    Pelicula buscarPelicula(int numeroDePelicula) throws IOException, InterruptedException {
+
+        URI direccion = URI.create("https://swapi.py4e.com/api/films/" + numeroDePelicula + "/");
 
 
-        HttpClient cleint =  HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder();
+        HttpClient client = HttpClient.newHttpClient();
 
-        .uri()
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
 
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        return new Gson().fromJson(response.body(), Pelicula.class);
     }
 }
