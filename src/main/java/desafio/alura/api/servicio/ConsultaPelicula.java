6b9500ba5824e2fec4a665d6a6b94887d4afab64
Daniel Romero <dandres508@gmail.com>
2024-09-30 17:41:17 -0500

    cambios

--- src/main/java/desafio/alura/api/servicio/ConsultaPelicula.java
+++ src/main/java/desafio/alura/api/servicio/ConsultaPelicula.java
@@ -3,28 +3,26 @@ package desafio.alura.api.servicio;
 import com.google.gson.Gson;
 import desafio.alura.api.modelos.Pelicula;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsultaPelicula {
-
-    Pelicula buscarPelicula(int numeroDePelicula) throws IOException, InterruptedException {
-
-        URI direccion = URI.create("https://swapi.py4e.com/api/films/" + numeroDePelicula + "/");
-
+    public Pelicula buscaPelicula(int numeroDePelicula) {
+        URI direccion = URI.create("https://swapi.dev/api/films/" + numeroDePelicula);
 
         HttpClient client = HttpClient.newHttpClient();
-
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(direccion)
                 .build();
 
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        return new Gson().fromJson(response.body(), Pelicula.class);
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Pelicula.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No encontré esa película.");
+        }
     }
 }
