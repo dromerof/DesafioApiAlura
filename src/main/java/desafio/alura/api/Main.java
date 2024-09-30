aa18679246f38f789dea04f23360616497be5484
Daniel Romero <dandres508@gmail.com>
2024-09-30 17:50:39 -0500

    Ultimos ajustes

--- src/main/java/desafio/alura/api/Main.java
+++ src/main/java/desafio/alura/api/Main.java
@@ -1,8 +1,26 @@
-package desafio.alura.api;
+import desafio.alura.api.modelos.Pelicula;
+import desafio.alura.api.servicio.ConsultaPelicula;
+import desafio.alura.api.servicio.GeneradorDeArchivos;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Scanner lectura = new Scanner(System.in);
+        ConsultaPelicula consulta = new ConsultaPelicula();
+        System.out.println("Escriba el numero de la pelicula de Star Wars que quiere consultar:");
+        try {
+            var numeroDePelicula = Integer.valueOf(lectura.nextLine());
+            Pelicula pelicula = consulta.buscaPelicula(numeroDePelicula);
+            System.out.println(pelicula);
+            GeneradorDeArchivos generador = new GeneradorDeArchivos(); // Aquí se crea el objeto
+            generador.guardarJson(pelicula);
+        } catch (NumberFormatException e) {
+            System.out.println("Número no encontrado " + e.getMessage());
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando la aplicación.");
+        }
     }
 }
--- src/main/java/desafio/alura/api/servicio/GeneradorDeArchivos.java
+++ src/main/java/desafio/alura/api/servicio/GeneradorDeArchivos.java
@@ -1,13 +1,13 @@
 package desafio.alura.api.servicio;
+import desafio.alura.api.modelos.Pelicula;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import desafio.alura.api.modelos.Pelicula;
 
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class GeneradorDeArchivo {
+public class GeneradorDeArchivos {
     public void guardarJson(Pelicula pelicula) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         FileWriter escritura = new FileWriter(pelicula.title()+".json");
